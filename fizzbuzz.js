74f3b63740289e9fbdc78092ebf30fc563413b15
Daniel Klein <danhirschklein@gmail.com>
2016-01-12 10:46:16 -0700

    Initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,7 @@
+for (var number = 1; number <= 100; number += 1) {
+  if (number%3===0) {console.log(fizz) 
+                    }
+  else if (number%5===0) console.log(buzz) 
+    
+    else console.log(number);
+}
